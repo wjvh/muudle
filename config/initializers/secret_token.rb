dd4fdb91d253b61fded22e982156e0d0012adada
Will Hanschell <island.vibe@gmail.com>
2011-02-25 19:10:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muudle::Application.config.secret_token = 'c9c8a56b88111536fd8bca9055206cd3563dfe3901076e8172ade837500e764502578eb190d7b778b7887d9109fe800b246257264c478ff8b938a018d630de8b'
