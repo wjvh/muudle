ec72593912f01daf6cda1889f9085dead0ecb576
Will Hanschell <island.vibe@gmail.com>
2011-02-25 22:55:36 -0400

    Updated mailserver settings

--- config/environments/env.rb
+++ config/environments/env.rb
@@ -4,10 +4,9 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "mail.muudle.com",
   :port                 => 26,
-  :domain               => 'muudle.com',
   :user_name            => 'muudlebot+muudle.com',
   :password             => 'baj_muudlebot!',
-  :authentication       => 'plain',
+  :authentication       => 'login',
   :enable_starttls_auto => true  }
 
 end
