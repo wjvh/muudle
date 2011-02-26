5f5fe33248d124edda9043d672fc984bbe35ddd5
Will Hanschell <island.vibe@gmail.com>
2011-02-25 23:18:46 -0400

    Tweaked mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,9 @@ Muudle::Application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "mail.muudle.com",
   :port                 => 26,
+  :domain               => 'muudle.com',
   :user_name            => 'muudlebot+muudle.com',
   :password             => 'baj_muudlebot!',
-  :authentication       => 'login',
+  :authentication       => :login,
   :enable_starttls_auto => true  }
 end
